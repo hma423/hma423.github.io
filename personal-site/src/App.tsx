c463c79b6b0b36cbbd72ab84774713f6ead33c53
carb0nn <henryma@ucsb.edu>
2024-07-06 15:11:54 -0700

    more s tuff

--- personal-site/src/App.tsx
+++ personal-site/src/App.tsx
@@ -9,6 +9,8 @@ import PDFViewer from './components/PDFViewer'
 import resume from "./Resume.png";
 import Projectprops from "./components/Projectprops";
 import Carousel from "./components/Carosuel";
+import introimage from "./assets/images/introimage.png"
+
 function App() {
 
   const tabs = [
@@ -16,12 +18,16 @@ function App() {
       title: "intro",
       value: "intro",
       content: (
-        <div className=" w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <p className = "font-bold mb-10">Hi! I'm Henry,</p>
-          <p className = "md:text-xl">I'm currently a junior stuyding Computer Science at Northwestern University. </p>
-          <p  className = "md:text-xl">Thanks for taking the time to look at my personal website. </p>
-          <p className = "md:text-xl" >I hope this can give a little more information about my interests and hobbies!</p>
+        <div className="flex flex-col justify-center items-center w-full h-full overflow-hidden rounded-2xl p-10 text-xl md:text-4xl text-white bg-gradient-to-br from-purple-700 to-violet-900">
+          <p className="font-bold mb-4">Hi! I'm Henry,</p>
+          <p className="text-xl">I'm currently a junior studying Computer Science at Northwestern University.</p>
+          <p className="text-xl">Thanks for taking the time to look at my personal website.</p>
+          <p className="mb-4 text-xl">I hope this can give a little more information about my interests and hobbies!</p>
+          <img src={introimage} alt="Henry" className="max-w-full max-h-full self-center" />
         </div>
+
+
+
       ),
     },
     {
@@ -49,24 +55,33 @@ function App() {
         </div>
       ),
     },
-    {
-      title: "Random",
-      value: "random",
-      content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <p>Random tab</p>
-        </div>
-      ),
-    },
+    // {
+    //   title: "designs",
+    //   value: "designs",
+    //   content: (
+    //     <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+    //       <p>designs</p>
+    //       <p className = "mt-4 -mb-16 text-sm">check out some cool designs ive made over the past few years</p>
+    //       <ImageGallery/>
+
+    //     </div>
+    //   ),
+    // },
   ];
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div className = "-mb-36">
+      <h1>henry ma</h1>
+      <h2>sometimes i code...</h2>
+    </div>
     <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items- justify-start my-40">
       <Tabs tabs={tabs} />
     </div>
+
     </> 
+
   )
 }
 
